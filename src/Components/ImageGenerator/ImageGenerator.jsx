30df438d6ec361e34c3f0fe2908dba35b081e4ac
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-06-27 21:36:36 +0530

    style added

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import './ImageGenerator.css'
+import React from "react";
+import "./ImageGenerator.css";
+import default_image from "../Assets/aig.gif";
 const ImageGenerator = () => {
   return (
-    <div>
-      
+    <div className="ai-image-generator">
+      <div className="header">
+        Imagica<span>-Get Your Image Here</span>
+      </div>
+      <div className="img-loading">
+        <div className="image">
+          <img src={default_image} alt="" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ImageGenerator
+export default ImageGenerator;
